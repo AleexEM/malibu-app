a2c79324290926e58c8771ba882196a0965dcefb
AleexEM <alexmeurer3@gmail.com>
2023-12-08 17:31:43 -0300

    a
    va

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,26 @@ export default function Home() {
   return (
     <main>
 
-    <header className='m-6'>
-      <h1>Logo</h1>
+    <header className='m-6 columns-4'>
+      <img
+      src="/buzios-02.png"
+      width={100}
+      height={100} 
+      alt="" />
+
+      <div className='font-serif font-black text-black '>
+        <h1>Artes da Galera</h1>
+        <h1>Minhas Artes</h1>
+        <h1>Vendas</h1>
+      </div>
+
     </header>
 
     <div>
     
+      
       <img  
-      src="/malibu.png" 
+      src="/buzios.png" 
       width={1920} 
       height={500}
       />
@@ -26,6 +38,18 @@ export default function Home() {
         Artes da Galera
       </h1>
       <h5>Espaço para você dar uma olhada em que o pessoal esta vendendo por aqui.</h5>
+    </div>
+
+
+
+    <div className='columns-4'>
+
+      <img src="/Pato Chapel.png" alt="" />
+      <img src="/Pato Ninja.png" alt="" />
+      <img src="/Pato Telemarketing.png" alt="" />
+      <img src="/Pato.png" alt="" />
+
+
     </div>
 
 
